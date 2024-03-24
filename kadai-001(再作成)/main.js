44b2defe227812c4a383c09a4844a7ba53d5e673
SA901105 <157731270+SA901105@users.noreply.github.com>
2024-03-24 18:13:59 +0900

    Add files via upload

--- kadai-001(再作成)/main.js
+++ kadai-001(再作成)/main.js
@@ -11,6 +11,7 @@ const typedfield = document.getElementById('typed');
 const wrap = document.getElementById('wrap');
 const start = document.getElementById('start');
 const count = document.getElementById('count');
+const count2 = document.getElementById('count2');
 
 // 複数のテキストを格納する配列
 const textLists = [
@@ -47,6 +48,8 @@ const createText = () => {
 // キー入力の判定
 const keyPress = e => {
 
+
+
   // 誤タイプの場合
   if(e.key !== untyped.substring(0, 1)) {
     wrap.classList.add('mistyped');
@@ -57,6 +60,8 @@ const keyPress = e => {
     return;
   }
 
+
+
   // 正タイプの場合
   // スコアのインクリメント
   score++;
@@ -64,24 +69,14 @@ const keyPress = e => {
   untyped = untyped.substring(1);
   typedfield.textContent = typed;
   untypedfield.textContent = untyped;
+  
 
+  // (課題001)正文字数
+  console.log(typed);
 
+  console.log(typed.length);
 
-  // （課題002）正タイプをした時にコンソールに文字数を表示
-  let letterCount = count2.textContent;
-  count2++;
-  count.textContent =
-  typed += untyped.substring(0, 1);
-  untyped = untyped.substring(1);
-  typedfield.textContent = typed;
-  untypedfield.textContent = untyped;
-
-  if(e.key === untyped.substring(0,1)) {
-
-  console.log(text.length + '文字');
-
-};
-
+  count2.textContent = typed.length;
 
 
   // テキストがなくなったら新しいテキストを表示
@@ -123,7 +118,7 @@ const gameOver = id => {
   }
 };
 
-// カウントダウンタイマー
+  // カウントダウンタイマー
 const timer = () => {
 
   // タイマー部分のHTML要素（p要素）を取得する
@@ -138,15 +133,19 @@ const timer = () => {
     // カウントが0になったらタイマーを停止する
     if(time <= 0) {
 
-      // (001課題)　タイマーが停止したら「タイムアップ！」と表示させる
+      // （課題002）タイマーが止まったら「typed」の範囲（文字表示）を非表示にする
+      document.getElementById("typed").style.display = "none";
+      document.getElementById("typed").style.display = "none";
+
+      // （課題002）タイマーが止まったら「タイムアップ！」と表示させる
       setTimeout(() => {
         untypedfield.textContent = 'タイムアップ！';
-      }, 100);
+      }, 1);
 
-      // タイマーが停止したらアラートを表示させる
+      // タイマーが止まったらアラートを表示させる
       setTimeout(() => {
         gameOver(id);
-      }, 200);
+      }, 100);
     }
   }, 1000);
 };
@@ -166,9 +165,6 @@ start.addEventListener('click', () => {
   // キーボードのイベント処理
   document.addEventListener('keypress', keyPress);
 
-  // （課題002）タイプ数カウントを開始する
-  lettercount();
 });
 
 untypedfield.textContent = 'スタートボタンで開始';
-
